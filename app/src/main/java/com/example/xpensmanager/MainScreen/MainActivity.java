b6104500e9247add6915a4197093b4d2310a57e5
Jitender <jitu.singh95@gmail.com>
2020-12-18 01:04:06 +0530

    Main Activity - Started

---
+++ app/src/main/java/com/example/xpensmanager/MainScreen/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.xpensmanager.MainScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.xpensmanager.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
