0b55b1eac1da0b77e5dc82750c8061ce092bfe09
Jitender <jitu.singh95@gmail.com>
2020-12-21 22:24:13 +0530

    Animations

--- app/src/main/java/com/example/xpensmanager/MainScreen/MainActivity.java
+++ app/src/main/java/com/example/xpensmanager/MainScreen/MainActivity.java
@@ -4,7 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import com.example.xpensmanager.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +21,116 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        List<String> forecast = new ArrayList<>();
+        forecast.add("Today - Sunny - 88/63");
+        forecast.add("Tomorrow - Foggy - 70/40");
+        forecast.add("Weds - Cloudy  - 72/63");
+        forecast.add("Thurs - Asteroids - 75/65");
+        forecast.add("Fri - Heavy Rain - 65/56");
+        forecast.add("Sat - HELP TRAPPED IN WEATHERSTATION - 60/51");
+        forecast.add("Sun - Sunny - 80/68");
+        forecast.add("Today - Sunny - 88/63");
+        forecast.add("Tomorrow - Foggy - 70/40");
+        forecast.add("Weds - Cloudy  - 72/63");
+        forecast.add("Thurs - Asteroids - 75/65");
+        forecast.add("Fri - Heavy Rain - 65/56");
+        forecast.add("Sat - HELP TRAPPED IN WEATHERSTATION - 60/51");
+        forecast.add("Sun - Sunny - 80/68");
+        forecast.add("Today - Sunny - 88/63");
+        forecast.add("Tomorrow - Foggy - 70/40");
+        forecast.add("Weds - Cloudy  - 72/63");
+        forecast.add("Thurs - Asteroids - 75/65");
+        forecast.add("Fri - Heavy Rain - 65/56");
+        forecast.add("Sat - HELP TRAPPED IN WEATHERSTATION - 60/51");
+        forecast.add("Sun - Sunny - 80/68");
+        forecast.add("Today - Sunny - 88/63");
+        forecast.add("Tomorrow - Foggy - 70/40");
+        forecast.add("Weds - Cloudy  - 72/63");
+        forecast.add("Thurs - Asteroids - 75/65");
+        forecast.add("Fri - Heavy Rain - 65/56");
+        forecast.add("Sat - HELP TRAPPED IN WEATHERSTATION - 60/51");
+        forecast.add("Sun - Sunny - 80/68");
+        forecast.add("Today - Sunny - 88/63");
+        forecast.add("Tomorrow - Foggy - 70/40");
+        forecast.add("Weds - Cloudy  - 72/63");
+        forecast.add("Thurs - Asteroids - 75/65");
+        forecast.add("Fri - Heavy Rain - 65/56");
+        forecast.add("Sat - HELP TRAPPED IN WEATHERSTATION - 60/51");
+        forecast.add("Sun - Sunny - 80/68");
+        forecast.add("Today - Sunny - 88/63");
+        forecast.add("Tomorrow - Foggy - 70/40");
+        forecast.add("Weds - Cloudy  - 72/63");
+        forecast.add("Thurs - Asteroids - 75/65");
+        forecast.add("Fri - Heavy Rain - 65/56");
+        forecast.add("Sat - HELP TRAPPED IN WEATHERSTATION - 60/51");
+        forecast.add("Sun - Sunny - 80/68");
+        forecast.add("Today - Sunny - 88/63");
+        forecast.add("Tomorrow - Foggy - 70/40");
+        forecast.add("Weds - Cloudy  - 72/63");
+        forecast.add("Thurs - Asteroids - 75/65");
+        forecast.add("Fri - Heavy Rain - 65/56");
+        forecast.add("Sat - HELP TRAPPED IN WEATHERSTATION - 60/51");
+        forecast.add("Sun - Sunny - 80/68");
+        forecast.add("Today - Sunny - 88/63");
+        forecast.add("Tomorrow - Foggy - 70/40");
+        forecast.add("Weds - Cloudy  - 72/63");
+        forecast.add("Thurs - Asteroids - 75/65");
+        forecast.add("Fri - Heavy Rain - 65/56");
+        forecast.add("Sat - HELP TRAPPED IN WEATHERSTATION - 60/51");
+        forecast.add("Sun - Sunny - 80/68");
+        forecast.add("Today - Sunny - 88/63");
+        forecast.add("Tomorrow - Foggy - 70/40");
+        forecast.add("Weds - Cloudy  - 72/63");
+        forecast.add("Thurs - Asteroids - 75/65");
+        forecast.add("Fri - Heavy Rain - 65/56");
+        forecast.add("Sat - HELP TRAPPED IN WEATHERSTATION - 60/51");
+        forecast.add("Sun - Sunny - 80/68");
+        forecast.add("Today - Sunny - 88/63");
+        forecast.add("Tomorrow - Foggy - 70/40");
+        forecast.add("Weds - Cloudy  - 72/63");
+        forecast.add("Thurs - Asteroids - 75/65");
+        forecast.add("Fri - Heavy Rain - 65/56");
+        forecast.add("Sat - HELP TRAPPED IN WEATHERSTATION - 60/51");
+        forecast.add("Sun - Sunny - 80/68");
+        forecast.add("Today - Sunny - 88/63");
+        forecast.add("Tomorrow - Foggy - 70/40");
+        forecast.add("Weds - Cloudy  - 72/63");
+        forecast.add("Thurs - Asteroids - 75/65");
+        forecast.add("Fri - Heavy Rain - 65/56");
+        forecast.add("Sat - HELP TRAPPED IN WEATHERSTATION - 60/51");
+        forecast.add("Sun - Sunny - 80/68");
+        forecast.add("Today - Sunny - 88/63");
+        forecast.add("Tomorrow - Foggy - 70/40");
+        forecast.add("Weds - Cloudy  - 72/63");
+        forecast.add("Thurs - Asteroids - 75/65");
+        forecast.add("Fri - Heavy Rain - 65/56");
+        forecast.add("Sat - HELP TRAPPED IN WEATHERSTATION - 60/51");
+        forecast.add("Sun - Sunny - 80/68");
+        forecast.add("Today - Sunny - 88/63");
+        forecast.add("Tomorrow - Foggy - 70/40");
+        forecast.add("Weds - Cloudy  - 72/63");
+        forecast.add("Thurs - Asteroids - 75/65");
+        forecast.add("Fri - Heavy Rain - 65/56");
+        forecast.add("Sat - HELP TRAPPED IN WEATHERSTATION - 60/51");
+        forecast.add("Sun - Sunny - 80/68");
+        forecast.add("Today - Sunny - 88/63");
+        forecast.add("Tomorrow - Foggy - 70/40");
+        forecast.add("Weds - Cloudy  - 72/63");
+        forecast.add("Thurs - Asteroids - 75/65");
+        forecast.add("Fri - Heavy Rain - 65/56");
+        forecast.add("Sat - HELP TRAPPED IN WEATHERSTATION - 60/51");
+        forecast.add("Sun - Sunny - 80/68");
+
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter(
+                getApplication(),
+                R.layout.view_adapter_main_activity,
+                R.id.text,
+                forecast
+        );
+        ListView listView = (ListView) findViewById(R.id.recyclerview);
+        listView.setAdapter(arrayAdapter);
 
     }
 }
