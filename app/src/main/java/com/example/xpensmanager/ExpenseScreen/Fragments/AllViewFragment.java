3a23f3d5f0e120e37fcc201bcbed4ce748d75925
Jitender <jitu.singh95@gmail.com>
2021-01-14 01:47:43 +0530

    Delete Expense

--- app/src/main/java/com/example/xpensmanager/ExpenseScreen/Fragments/AllViewFragment.java
+++ app/src/main/java/com/example/xpensmanager/ExpenseScreen/Fragments/AllViewFragment.java
@@ -2,23 +2,32 @@ package com.example.xpensmanager.ExpenseScreen.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.content.res.AppCompatResources;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.example.xpensmanager.Database.CategoryDB;
 import com.example.xpensmanager.Database.ExpenseDB;
+import com.example.xpensmanager.Database.GroupDB;
 import com.example.xpensmanager.Enums.ViewType;
 import com.example.xpensmanager.Database.ExpenseData;
 import com.example.xpensmanager.ExpenseScreen.Adapters.ExpenseViewAdapter;
+import com.example.xpensmanager.MainScreen.Adapters.SwipeToDeleteCallback;
 import com.example.xpensmanager.R;
 import com.example.xpensmanager.SplashScreen.SplashScreenActivity;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,11 +42,17 @@ import xyz.sangcomz.stickytimelineview.model.SectionInfo;
  * create an instance of this fragment.
  */
 public class AllViewFragment extends Fragment {
+    private CategoryDB categoryDB;
+    private GroupDB groupsDB;
     private ExpenseDB expenseDB;
     private String tableName,groupBy,filterType;
     private int filterValue;
     private TextView allTimeTotalSpends;
     private ArrayList<ExpenseData> expenseData;
+    public static RelativeLayout emptyView;
+    private CoordinatorLayout framelayout;
+    private ExpenseViewAdapter adapter;
+    private TimeLineRecyclerView recyclerView;
 
     public AllViewFragment() {
         // Required empty public constructor
@@ -64,10 +79,14 @@ public class AllViewFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_all_view, container, false);
-        TimeLineRecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView = view.findViewById(R.id.recycler_view);
         allTimeTotalSpends = view.findViewById(R.id.allTimeTotalSpends);
+        emptyView = view.findViewById(R.id.emptyView);
+        framelayout = view.findViewById(R.id.frameLayout);
 
         expenseDB = new ExpenseDB(getActivity());
+        groupsDB = new GroupDB(getActivity());
+        categoryDB = new CategoryDB(getActivity());
 
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(),
                 RecyclerView.VERTICAL,
@@ -89,12 +108,81 @@ public class AllViewFragment extends Fragment {
                 allTimeTotalSpends.setText(SplashScreenActivity.cSymbol+ " "+totalSpends);
             }
         }
-
-
+        if(expenseData.size() == 0) {
+            emptyView.setVisibility(View.VISIBLE);
+        }else{
+            emptyView.setVisibility(View.GONE);
+        }
+        adapter = new ExpenseViewAdapter(expenseData,getActivity(), ViewType.ALL);
         recyclerView.addItemDecoration(getSectionCallback(expenseData,groupBy));
-        recyclerView.setAdapter(new ExpenseViewAdapter(expenseData,getActivity(), ViewType.ALL));
+        recyclerView.setAdapter(adapter);
+        //enableSwipeToDeleteAndUndo();
         return view;
     }
+
+    private void enableSwipeToDeleteAndUndo() {
+        SwipeToDeleteCallback swipeToDeleteCallback = new SwipeToDeleteCallback(getActivity()) {
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+                final int position = viewHolder.getAdapterPosition();
+                final ExpenseData item = adapter.getData().get(position);
+                Log.d("Snackbar",item.toString());
+                adapter.removeItem(position);
+                if(adapter.getData().size() == 0) {
+                    emptyView.setVisibility(View.VISIBLE);
+                }else{
+                    emptyView.setVisibility(View.GONE);
+                }
+                Snackbar snackbar = Snackbar
+                        .make(framelayout, "Item was removed from the list",10000)
+                        .addCallback(new Snackbar.Callback(){
+
+                            @Override
+                            public void onShown(Snackbar sb) {
+                                super.onShown(sb);
+                                //Toast.makeText(getActivity(),"Snack bar shown",Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onDismissed(Snackbar transientBottomBar, int event) {
+                                super.onDismissed(transientBottomBar, event);
+                                if (event == Snackbar.Callback.DISMISS_EVENT_TIMEOUT || event == Snackbar.Callback.DISMISS_EVENT_SWIPE ||
+                                        event == Snackbar.Callback.DISMISS_EVENT_MANUAL || event == Snackbar.Callback.DISMISS_EVENT_CONSECUTIVE) {
+                                    // Snackbar closed on its own or swiped to close
+                                    double netAmount = groupsDB.getNetAmountByTitle(item.getGroup());
+                                    double totalAmount = groupsDB.getTotalAmountByTitle(item.getGroup());
+                                    double totalCategoryAmount = categoryDB.getTotalAmountByTitle(item.getCategory());
+                                    totalAmount = totalAmount - item.getAmount();
+                                    if(item.getPaidBy().equalsIgnoreCase("Me")) {
+                                        netAmount = netAmount - (item.getAmount()-item.getSplitAmount());
+                                    }else{
+                                        netAmount = netAmount + item.getSplitAmount();
+                                    }
+                                    groupsDB.updateGroupAmountByTitle(item.getGroup(),netAmount,totalAmount);
+                                    categoryDB.updateCategoryAmountByTitle(item.getCategory(),(totalCategoryAmount-item.getSplitAmount()));
+                                    expenseDB.deleteExpenseById(item.getId());
+                                }
+                            }
+                        });
+                snackbar.setAction("UNDO", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        adapter.restoreItem(item, position);
+                        recyclerView.scrollToPosition(position);
+                        if(emptyView.getVisibility() == View.VISIBLE) {
+                            emptyView.setVisibility(View.GONE);
+                        }
+                    }
+                });
+                snackbar.setActionTextColor(getActivity().getResources().getColor(R.color.theme_yellow));
+                snackbar.show();
+            }
+        };
+
+        ItemTouchHelper itemTouchhelper = new ItemTouchHelper(swipeToDeleteCallback);
+        itemTouchhelper.attachToRecyclerView(recyclerView);
+    }
+
 
     private SectionCallback getSectionCallback(final List<ExpenseData> expenseData, String groupBy) {
         return new SectionCallback() {
