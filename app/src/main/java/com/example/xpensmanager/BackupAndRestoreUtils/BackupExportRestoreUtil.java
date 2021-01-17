f0c504d9100e5b773ce9578cb1d83d355f0b8b51
Jitender <jitu.singh95@gmail.com>
2021-01-18 00:53:02 +0530

    Upload to google drive

--- app/src/main/java/com/example/xpensmanager/BackupAndRestoreUtils/BackupExportRestoreUtil.java
+++ app/src/main/java/com/example/xpensmanager/BackupAndRestoreUtils/BackupExportRestoreUtil.java
@@ -2,9 +2,12 @@ package com.example.xpensmanager.BackupAndRestoreUtils;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Environment;
 import android.util.Log;
 import android.widget.Toast;
+
+import androidx.core.content.FileProvider;
 
 import com.example.xpensmanager.Database.CategoryDB;
 import com.example.xpensmanager.Database.CategoryData;
@@ -21,10 +24,12 @@ import org.apache.poi.ss.usermodel.Cell;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
@@ -79,49 +84,7 @@ public class BackupExportRestoreUtil {
     }
 
     public String createBackUp() {
-        StringBuffer data = new StringBuffer();
-        data.append("Tablename -:- " + ExpenseDB.tableName);
-        data.append("\n***START***\n");
-        for (int i = 0; i < expenseData.size(); i++) {
-            data.append(expenseData.get(i).getId() + "||" +
-                    expenseData.get(i).getDate() + "||" +
-                    expenseData.get(i).getAmount() + "||" +
-                    expenseData.get(i).getDescription() + "||" +
-                    expenseData.get(i).getPaidBy() + "||" +
-                    expenseData.get(i).getCategory() + "||" +
-                    expenseData.get(i).getSplitAmount() + "||" +
-                    expenseData.get(i).getGroup() + "\n");
-        }
-        data.append("***END***\n");
-        data.append("Tablename -:- " + GroupDB.groupdb_table);
-        data.append("\n***START***\n");
-        for (int i = 0; i < groupData.size(); i++) {
-            data.append(groupData.get(i).getId() + "||" +
-                    groupData.get(i).getTitle() + "||" +
-                    groupData.get(i).getNoOfPersons() + "||" +
-                    groupData.get(i).getMaxLimit() + "||" +
-                    groupData.get(i).getNetAmount() + "||" +
-                    groupData.get(i).getTotalAmount() + "\n");
-        }
-        data.append("***END***\n");
-        data.append("Tablename -:- " + CategoryDB.categorydb_table);
-        data.append("\n***START***\n");
-        for (int i = 0; i < categoryData.size(); i++) {
-            data.append(categoryData.get(i).getId() + "||" +
-                    categoryData.get(i).getCategory() + "||" +
-                    categoryData.get(i).getLimit() + "||" +
-                    categoryData.get(i).getTotalCategorySpend() + "\n");
-        }
-        data.append("***END***\n");
-        Map<String,?> sharedPref = sharedPreferences.getAll();
-        data.append("Tablename -:- Shared-Preferences");
-        data.append("\n***START***\n");
-        for(String key : sharedPref.keySet()){
-            data.append(key+"||"+sharedPref.get(key).toString()+"||"+sharedPref.get(key).getClass().toString()+"\n");
-        }
-        data.append("***END***\n");
-        Log.d("Backup data - ", String.valueOf(data));
-        String encrypted = encrypt.encryptOrNull(String.valueOf(data));
+        String encrypted = encrypt.encryptOrNull(String.valueOf(backup()));
         try {
             if (isExternalStorageWritable() && isExternalStorageReadable()) {
                 File dir = new File(context.getExternalFilesDir(null).getAbsolutePath(),"/XpensManager/Backup/");
@@ -327,70 +290,8 @@ public class BackupExportRestoreUtil {
                 if (!output.exists()) {
                     return "Backup doesn't exist";
                 } else {
-                    BufferedReader result = new BufferedReader(new FileReader(output));
-                    String temp="";
-                    String x;
-                    while((x = result.readLine())!=null){ temp +=x; }
-                    String tempData = encrypt.decryptOrNull(temp);
-                    BufferedReader data = new BufferedReader(new StringReader(tempData));
-                    String line;
-                    int flag = -1;
-                    while((line = data.readLine())!=null){
-                        if( !(line.contains("***END***") || line.contains("***START***")) ) {
-                            if (line.contains("Tablename -:- ")) {
-                                String tablename = line.split("-:-")[1].trim();
-                                if (tablename.equals(ExpenseDB.tableName)) {
-                                    flag = 0;
-                                    expenseDB.deleteAll();
-                                } else if (tablename.equals(GroupDB.groupdb_table)) {
-                                    flag = 1;
-                                    groupDB.deleteAll();
-                                } else if (tablename.equals(CategoryDB.categorydb_table)) {
-                                    flag =2;
-                                    categoryDB.deleteAll();
-                                } else if (tablename.equals("Shared-Preferences")) {
-                                    Log.d("Restore Data", "Shared Preferences");
-                                    flag = 3;
-                                } else {
-                                    Log.d("Restore Data", "Some Issue in data");
-                                }
-                            }else {
-                                System.out.println(line);
-                                String[] actualData = line.split("\\|\\|");
-                                if(flag == 0)
-                                    expenseDB.insertNewExpenseFromBackup(new SimpleDateFormat("dd/MM/yyyy").parse(actualData[1]), Double.parseDouble(actualData[2]), actualData[3], actualData[5], actualData[4], Double.parseDouble(actualData[6]), actualData[7]);
-                                else if(flag == 1)
-                                    groupDB.insertNewGroupFromBackup(actualData[1], Integer.parseInt(actualData[2]), Double.parseDouble(actualData[3]),  Double.parseDouble(actualData[4]), Double.parseDouble(actualData[5]));
-                                else if(flag == 2)
-                                    categoryDB.insertNewCategoryFromBackup(actualData[1], Double.parseDouble(actualData[2]), Double.parseDouble(actualData[3]));
-                                else if(flag == 3){
-                                    //shared preferences
-                                    if(actualData[2].contains("Boolean")){
-                                        editor.putBoolean(actualData[0],Boolean.parseBoolean(actualData[1]));
-                                        editor.apply();
-                                    }else if(actualData[2].contains("String")){
-                                        editor.putString(actualData[0],actualData[1]);
-                                        editor.apply();
-                                    }else if(actualData[2].contains("Integer")){
-                                        editor.putInt(actualData[0],Integer.parseInt(actualData[1]));
-                                        editor.apply();
-                                    }else if(actualData[2].contains("Long")){
-                                        editor.putLong(actualData[0],Long.parseLong(actualData[1]));
-                                        editor.apply();
-                                    }else if(actualData[2].contains("Float")){
-                                        editor.putFloat(actualData[0],Float.parseFloat(actualData[1]));
-                                        editor.apply();
-                                    }else{
-                                        //Ignore the row.
-                                        Log.d("Shared_preferences","Error in restoring shared-preferences");
-                                    }
-                                }
-                            }
-
-                        }
-                    }
-                    data.close();
-                    return "Successfully restored data from backup";
+                   //Code to restore from backup
+                    return restoreFromFile(output);
                 }
 
             } else {
@@ -435,4 +336,160 @@ public class BackupExportRestoreUtil {
             return false;
         }
     }
+
+    public Uri getBackupFilePath() {
+        File dir = new File(context.getExternalFilesDir(null).getAbsolutePath(), "/XpensManager/Backup/");
+        File output = new File(dir, filename);
+        Uri uri = FileProvider.getUriForFile(context,context.getApplicationContext().getPackageName()+".provider",output);//Uri.fromFile(output);
+        return uri;
+    }
+
+    public String restoreFromFile(File output) throws IOException,ParseException {
+        try {
+            BufferedReader result = new BufferedReader(new FileReader(output));
+            String temp = "";
+            String x;
+            while ((x = result.readLine()) != null) {
+                temp += x;
+            }
+            return restore(temp);
+        }catch (Exception e) {
+            Log.d("Restore Error",e.toString());
+            return "Some error while reading from file";
+        }
+    }
+
+    public String restoreFromUri(Uri uri) throws IOException,ParseException {
+        try {
+            BufferedReader result = new BufferedReader(new InputStreamReader(context.getContentResolver().openInputStream(uri)));
+            String temp = "";
+            String x;
+            while ((x = result.readLine()) != null) {
+                temp += x;
+            }
+            return restore(temp);
+        }catch (Exception e) {
+            Log.d("Restore Error",e.toString());
+            return "Unable to restore. Some error while reading from file";
+        }
+    }
+
+    private String restore(String temp) throws IOException,ParseException{
+        //Log.d("FileData",temp);
+        String tempData = encrypt.decryptOrNull(temp);
+        Log.d("FileData",tempData);
+        BufferedReader data = new BufferedReader(new StringReader(tempData));
+        String line;
+        int flag = -1;
+        if (data.readLine().equals("<Xpens_Manager_Backup>")) {
+            while ((line = data.readLine()) != null) {
+                if (!(line.contains("***END***") || line.contains("***START***"))) {
+                    if (line.contains("Tablename -:- ")) {
+                        String tablename = line.split("-:-")[1].trim();
+                        if (tablename.equals(ExpenseDB.tableName)) {
+                            flag = 0;
+                            expenseDB.deleteAll();
+                        } else if (tablename.equals(GroupDB.groupdb_table)) {
+                            flag = 1;
+                            groupDB.deleteAll();
+                        } else if (tablename.equals(CategoryDB.categorydb_table)) {
+                            flag = 2;
+                            categoryDB.deleteAll();
+                        } else if (tablename.equals("Shared-Preferences")) {
+                            Log.d("Restore Data", "Shared Preferences");
+                            flag = 3;
+                        } else {
+                            Log.d("Restore Data", "Some Issue in data");
+                        }
+                    } else {
+                        System.out.println(line);
+                        String[] actualData = line.split("\\|\\|");
+                        if (flag == 0)
+                            expenseDB.insertNewExpenseFromBackup(new SimpleDateFormat("dd/MM/yyyy").parse(actualData[1]), Double.parseDouble(actualData[2]), actualData[3], actualData[5], actualData[4], Double.parseDouble(actualData[6]), actualData[7]);
+                        else if (flag == 1)
+                            groupDB.insertNewGroupFromBackup(actualData[1], Integer.parseInt(actualData[2]), Double.parseDouble(actualData[3]), Double.parseDouble(actualData[4]), Double.parseDouble(actualData[5]));
+                        else if (flag == 2)
+                            categoryDB.insertNewCategoryFromBackup(actualData[1], Double.parseDouble(actualData[2]), Double.parseDouble(actualData[3]));
+                        else if (flag == 3) {
+                            //shared preferences
+                            if (actualData[2].contains("Boolean")) {
+                                editor.putBoolean(actualData[0], Boolean.parseBoolean(actualData[1]));
+                                editor.apply();
+                            } else if (actualData[2].contains("String")) {
+                                editor.putString(actualData[0], actualData[1]);
+                                editor.apply();
+                            } else if (actualData[2].contains("Integer")) {
+                                editor.putInt(actualData[0], Integer.parseInt(actualData[1]));
+                                editor.apply();
+                            } else if (actualData[2].contains("Long")) {
+                                editor.putLong(actualData[0], Long.parseLong(actualData[1]));
+                                editor.apply();
+                            } else if (actualData[2].contains("Float")) {
+                                editor.putFloat(actualData[0], Float.parseFloat(actualData[1]));
+                                editor.apply();
+                            } else {
+                                //Ignore the row.
+                                Log.d("Shared_preferences", "Error in restoring shared-preferences");
+                            }
+                        }
+                    }
+
+                }
+            }
+            data.close();
+            return "Successfully restored data from backup";
+        } else {
+            data.close();
+            return "Either backup file is corrupted or wrong backup file selected";
+        }
+    }
+
+    private StringBuffer backup(){
+        StringBuffer data = new StringBuffer();
+        data.append("<Xpens_Manager_Backup>\n");
+        data.append("Tablename -:- " + ExpenseDB.tableName);
+        data.append("\n***START***\n");
+        for (int i = 0; i < expenseData.size(); i++) {
+            data.append(expenseData.get(i).getId() + "||" +
+                    expenseData.get(i).getDate() + "||" +
+                    expenseData.get(i).getAmount() + "||" +
+                    expenseData.get(i).getDescription() + "||" +
+                    expenseData.get(i).getPaidBy() + "||" +
+                    expenseData.get(i).getCategory() + "||" +
+                    expenseData.get(i).getSplitAmount() + "||" +
+                    expenseData.get(i).getGroup() + "\n");
+        }
+        data.append("***END***\n");
+        data.append("Tablename -:- " + GroupDB.groupdb_table);
+        data.append("\n***START***\n");
+        for (int i = 0; i < groupData.size(); i++) {
+            data.append(groupData.get(i).getId() + "||" +
+                    groupData.get(i).getTitle() + "||" +
+                    groupData.get(i).getNoOfPersons() + "||" +
+                    groupData.get(i).getMaxLimit() + "||" +
+                    groupData.get(i).getNetAmount() + "||" +
+                    groupData.get(i).getTotalAmount() + "\n");
+        }
+        data.append("***END***\n");
+        data.append("Tablename -:- " + CategoryDB.categorydb_table);
+        data.append("\n***START***\n");
+        for (int i = 0; i < categoryData.size(); i++) {
+            data.append(categoryData.get(i).getId() + "||" +
+                    categoryData.get(i).getCategory() + "||" +
+                    categoryData.get(i).getLimit() + "||" +
+                    categoryData.get(i).getTotalCategorySpend() + "\n");
+        }
+        data.append("***END***\n");
+        Map<String,?> sharedPref = sharedPreferences.getAll();
+        data.append("Tablename -:- Shared-Preferences");
+        data.append("\n***START***\n");
+        for(String key : sharedPref.keySet()){
+            data.append(key+"||"+sharedPref.get(key).toString()+"||"+sharedPref.get(key).getClass().toString()+"\n");
+        }
+        data.append("***END***\n");
+        Log.d("Backup data - ", String.valueOf(data));
+        return data;
+    }
+
+
 }
