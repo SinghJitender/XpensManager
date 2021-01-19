5d381b986cab138ec37488d176d6f1f485fe36be
Jitender <jitu.singh95@gmail.com>
2021-01-19 12:38:59 +0530

    Fixes

--- app/src/main/java/com/example/xpensmanager/ExpenseScreen/Fragments/MonthlyViewFragment.java
+++ app/src/main/java/com/example/xpensmanager/ExpenseScreen/Fragments/MonthlyViewFragment.java
@@ -2,9 +2,12 @@ package com.example.xpensmanager.ExpenseScreen.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.content.res.AppCompatResources;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -12,7 +15,9 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.xpensmanager.Database.CategoryDB;
@@ -21,8 +26,10 @@ import com.example.xpensmanager.Database.GroupDB;
 import com.example.xpensmanager.Enums.ViewType;
 import com.example.xpensmanager.Database.ExpenseData;
 import com.example.xpensmanager.ExpenseScreen.Adapters.ExpenseViewAdapter;
+import com.example.xpensmanager.MainScreen.Adapters.SwipeToDeleteCallback;
 import com.example.xpensmanager.R;
 import com.example.xpensmanager.SplashScreen.SplashScreenActivity;
+import com.google.android.material.snackbar.Snackbar;
 import com.whiteelephant.monthpicker.MonthPickerDialog;
 
 import java.text.DecimalFormat;
@@ -42,6 +49,7 @@ import xyz.sangcomz.stickytimelineview.model.SectionInfo;
  * create an instance of this fragment.
  */
 public class MonthlyViewFragment extends Fragment {
+    private CategoryDB categoryDB;
     private ExpenseDB expenseDB;
     private String tableName,groupBy,filterType;
     private int filterValue;
@@ -52,9 +60,11 @@ public class MonthlyViewFragment extends Fragment {
     private TimeLineRecyclerView recyclerView;
     private ExpenseViewAdapter adapter;
     private ProgressBar progressBar;
-    private CategoryDB categoryDB;
     private GroupDB groupDB;
     private double totalCategorySum, categoryLimit, groupLimit;
+    public static RelativeLayout emptyView;
+    private CoordinatorLayout framelayout;
+    private FrameLayout monthlyContainer;
 
     public MonthlyViewFragment() {
         // Required empty public constructor
@@ -88,6 +98,9 @@ public class MonthlyViewFragment extends Fragment {
         currentMonthTotalSpends = view.findViewById(R.id.currentMonthTotalSpends);
         progressBar = view.findViewById(R.id.progress);
         limit = view.findViewById(R.id.limit);
+        emptyView = view.findViewById(R.id.emptyView);
+        framelayout = view.findViewById(R.id.frameLayout);
+        monthlyContainer = view.findViewById(R.id.monthly_container);
 
         expenseDB = new ExpenseDB(getActivity());
         categoryDB = new CategoryDB(getActivity());
@@ -129,10 +142,15 @@ public class MonthlyViewFragment extends Fragment {
                 limit.setText(new DecimalFormat("00.00").format(totalSpendsThisMonth) + "/" + new DecimalFormat("00.00").format(groupLimit));
             }
         }
-
+        if(expenseData.size() == 0) {
+            emptyView.setVisibility(View.VISIBLE);
+        }else{
+            emptyView.setVisibility(View.GONE);
+        }
         adapter = new ExpenseViewAdapter(expenseData,getActivity(), ViewType.MONTHLY);
         recyclerView.addItemDecoration(getSectionCallback(expenseData));
         recyclerView.setAdapter(adapter);
+        enableSwipeToDeleteAndUndo();
         return view;
     }
     private SectionCallback getSectionCallback(final List<ExpenseData> d) {
@@ -149,6 +167,90 @@ public class MonthlyViewFragment extends Fragment {
                 return !d.get(position).getDate().equals(d.get(position - 1).getDate());
             }
         };
+    }
+
+    private void enableSwipeToDeleteAndUndo() {
+        SwipeToDeleteCallback swipeToDeleteCallback = new SwipeToDeleteCallback(getActivity()) {
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+                final int position = viewHolder.getAdapterPosition();
+                final ExpenseData item = adapter.getData().get(position);
+                Log.d("Snackbar",item.toString());
+                adapter.removeItem(position);
+                if(adapter.getData().size() == 0) {
+                    emptyView.setVisibility(View.VISIBLE);
+                }else{
+                    emptyView.setVisibility(View.GONE);
+                }
+                Snackbar snackbar = Snackbar
+                        .make(monthlyContainer, "Item was removed from the list",10000)
+                        .addCallback(new Snackbar.Callback(){
+
+                            @Override
+                            public void onShown(Snackbar sb) {
+                                super.onShown(sb);
+                                //Toast.makeText(getActivity(),"Snack bar shown",Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onDismissed(Snackbar transientBottomBar, int event) {
+                                super.onDismissed(transientBottomBar, event);
+                                if (event == Snackbar.Callback.DISMISS_EVENT_TIMEOUT || event == Snackbar.Callback.DISMISS_EVENT_SWIPE ||
+                                        event == Snackbar.Callback.DISMISS_EVENT_MANUAL || event == Snackbar.Callback.DISMISS_EVENT_CONSECUTIVE) {
+                                    // Snackbar closed on its own or swiped to close
+                                    double netAmount = groupDB.getNetAmountByTitle(item.getGroup());
+                                    double totalAmount = groupDB.getTotalAmountByTitle(item.getGroup());
+                                    double totalCategoryAmount = categoryDB.getTotalAmountByTitle(item.getCategory());
+                                    totalAmount = totalAmount - item.getAmount();
+                                    if(item.getPaidBy().equalsIgnoreCase("Me")) {
+                                        netAmount = netAmount - (item.getAmount()-item.getSplitAmount());
+                                    }else{
+                                        netAmount = netAmount + item.getSplitAmount();
+                                    }
+                                    groupDB.updateGroupAmountByTitle(item.getGroup(),netAmount,totalAmount);
+                                    categoryDB.updateCategoryAmountByTitle(item.getCategory(),(totalCategoryAmount-item.getSplitAmount()));
+                                    expenseDB.deleteExpenseById(item.getId());
+                                    if(groupBy.equalsIgnoreCase("None")) {
+                                        double totalSpendsThisMonth = expenseDB.getMonthlyExpenseSum(ExpenseDB.getMonthFromDate(new Date()), ExpenseDB.getYearFromDate(new Date()));
+                                        currentMonthTotalSpends.setText(SplashScreenActivity.cSymbol+ " "+ totalSpendsThisMonth);
+                                        progressBar.setProgress((int) ((totalSpendsThisMonth / totalCategorySum) * 100));
+                                        limit.setText(new DecimalFormat("00.00").format(totalSpendsThisMonth) + "/" + new DecimalFormat("00.00").format(totalCategorySum));
+                                    }
+                                    else{
+                                        if(filterType.equalsIgnoreCase("category")) {
+                                            double totalSpendsThisMonth = expenseDB.getMonthlyExpenseSumByCategory(ExpenseDB.getMonthFromDate(new Date()), ExpenseDB.getYearFromDate(new Date()),groupBy);
+                                            currentMonthTotalSpends.setText(SplashScreenActivity.cSymbol+ " "+ totalSpendsThisMonth);
+                                            categoryLimit = categoryDB.getCategoryLimitByTitle(groupBy);
+                                            progressBar.setProgress((int) ((totalSpendsThisMonth / categoryLimit) * 100));
+                                            limit.setText(new DecimalFormat("00.00").format(totalSpendsThisMonth) + "/" + new DecimalFormat("00.00").format(categoryLimit));
+                                        }else{
+                                            double totalSpendsThisMonth = expenseDB.getMonthlyExpenseSumByGroup(ExpenseDB.getMonthFromDate(new Date()), ExpenseDB.getYearFromDate(new Date()),groupBy);
+                                            currentMonthTotalSpends.setText(SplashScreenActivity.cSymbol+ " "+ totalSpendsThisMonth);
+                                            groupLimit = groupDB.getGroupLimitByTitle(groupBy);
+                                            progressBar.setProgress((int) ((totalSpendsThisMonth / groupLimit) * 100));
+                                            limit.setText(new DecimalFormat("00.00").format(totalSpendsThisMonth) + "/" + new DecimalFormat("00.00").format(groupLimit));
+                                        }
+                                    }
+                                }
+                            }
+                        });
+                snackbar.setAction("UNDO", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        adapter.restoreItem(item, position);
+                        recyclerView.scrollToPosition(position);
+                        if(emptyView.getVisibility() == View.VISIBLE) {
+                            emptyView.setVisibility(View.GONE);
+                        }
+                    }
+                });
+                snackbar.setActionTextColor(getActivity().getResources().getColor(R.color.theme_yellow));
+                snackbar.show();
+            }
+        };
+
+        ItemTouchHelper itemTouchhelper = new ItemTouchHelper(swipeToDeleteCallback);
+        itemTouchhelper.attachToRecyclerView(recyclerView);
     }
 
     public void displayGroupDialogBox(){
@@ -178,6 +280,11 @@ public class MonthlyViewFragment extends Fragment {
                                     progressBar.setProgress((int) ((totalSpendsThisMonth / groupLimit) * 100));
                                     limit.setText(new DecimalFormat("00.00").format(totalSpendsThisMonth) + "/" + new DecimalFormat("00.00").format(groupLimit));
                                 }
+                            }
+                            if(expenseData.size() == 0) {
+                                emptyView.setVisibility(View.VISIBLE);
+                            }else{
+                                emptyView.setVisibility(View.GONE);
                             }
                             adapter.notifyDataSetChanged();
                     }
