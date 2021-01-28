6188912258018f8c19a2af0f784f0f7643d1443b
Jitender <jitu.singh95@gmail.com>
2021-01-28 14:23:00 +0530

    Fixes and Improvements

---
+++ app/src/main/java/com/jitender/xpensmanager/Database/CategoryData.java
@@ -0,0 +1,47 @@
+package com.jitender.xpensmanager.Database;
+
+public class CategoryData {
+    private int id;
+    private String category;
+    private double limit;
+    private double totalCategorySpend;
+
+    public CategoryData(int id, String category, double limit, double totalCategorySpend) {
+        this.id = id;
+        this.category = category;
+        this.limit = limit;
+        this.totalCategorySpend = totalCategorySpend;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+
+    public void setLimit(double limit) {
+        this.limit = limit;
+    }
+
+    public double getTotalCategorySpend() {
+        return totalCategorySpend;
+    }
+
+    public void setTotalCategorySpend(double totalCategorySpend) {
+        this.totalCategorySpend = totalCategorySpend;
+    }
+}
