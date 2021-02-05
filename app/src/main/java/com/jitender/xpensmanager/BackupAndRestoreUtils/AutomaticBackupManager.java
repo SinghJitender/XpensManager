dbf5b6be927d4272770511f43d76af7974f1a3b2
Jitender <jitu.singh95@gmail.com>
2021-02-05 16:22:18 +0530

    Fixes

--- app/src/main/java/com/jitender/xpensmanager/BackupAndRestoreUtils/AutomaticBackupManager.java
+++ app/src/main/java/com/jitender/xpensmanager/BackupAndRestoreUtils/AutomaticBackupManager.java
@@ -2,19 +2,21 @@ package com.jitender.xpensmanager.BackupAndRestoreUtils;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.jitender.xpensmanager.R;
 
 import java.util.Calendar;
 
-public class AutomaticBackupManager {
-
+public class AutomaticBackupManager extends BroadcastReceiver {
 
     public void setAutomaticBackup(Context context){
         ComponentName receiver = new ComponentName(context, AutomaticBackup.class);
@@ -33,13 +35,12 @@ public class AutomaticBackupManager {
         calendar.set(Calendar.MINUTE,0);
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(context,AutomaticBackup.class);
-        PendingIntent pendingIntent = PendingIntent.getService(context,0,intent,PendingIntent.FLAG_ONE_SHOT);
-        am.set(AlarmManager.RTC,System.currentTimeMillis(),pendingIntent);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,1330,intent,PendingIntent.FLAG_ONE_SHOT);
+        //am.set(AlarmManager.RTC,System.currentTimeMillis(),pendingIntent);
 
-       /* //{"None","Daily","Weekly","Monthly"}
+        //{"None","Daily","Weekly","Monthly"}
         if(preference.equalsIgnoreCase("Daily")){
-            am.setRepeating(AlarmManager.RTC,System.currentTimeMillis()+3000,1000*60,pendingIntent);
-            //am.setRepeating(AlarmManager.RTC,System.currentTimeMillis(),1000*60*60*24,pendingIntent);
+            am.setRepeating(AlarmManager.RTC,System.currentTimeMillis(),1000*60*60*24,pendingIntent);
         }
         else if(preference.equalsIgnoreCase("Weekly")) {
             am.setRepeating(AlarmManager.RTC, System.currentTimeMillis(), 1000 * 60 * 60 * 24 * 7, pendingIntent);
@@ -49,10 +50,11 @@ public class AutomaticBackupManager {
         }
         else {
             cancelAutomaticBackup(context);
-        }*/
+        }
+        Toast.makeText(context,"Automatic backup set to : "+preference,Toast.LENGTH_SHORT).show();
     }
 
-   /* public void cancelAutomaticBackup(Context context){
+    public void cancelAutomaticBackup(Context context){
         ComponentName receiver = new ComponentName(context, AutomaticBackup.class);
         PackageManager pm = context.getPackageManager();
 
@@ -61,8 +63,12 @@ public class AutomaticBackupManager {
                 PackageManager.DONT_KILL_APP);
         AlarmManager am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(context,AutomaticBackup.class);
-        PendingIntent pendingIntent = PendingIntent.getService(context,0,intent,PendingIntent.FLAG_NO_CREATE);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context,1330,intent,PendingIntent.FLAG_CANCEL_CURRENT);
         am.cancel(pendingIntent);
-    }*/
+    }
 
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        setAutomaticBackup(context);
+    }
 }
