557028a23084f134d1683ba609e79bcde16fd9be
Jitender <jitu.singh95@gmail.com>
2021-02-09 11:15:08 +0530

    Added new activity - FAQ's

---
+++ app/src/main/java/com/jitender/xpensmanager/SettingScreen/UserGuide.java
@@ -0,0 +1,16 @@
+package com.jitender.xpensmanager.SettingScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jitender.xpensmanager.R;
+
+public class UserGuide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_guide);
+    }
+}
