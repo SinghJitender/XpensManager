a87287e05412aa1435b47e972462b6669732c680
Jitender <jitu.singh95@gmail.com>
2021-02-10 11:21:34 +0530

    Alarm Receiver Updates

--- app/src/main/java/com/jitender/xpensmanager/BackupAndRestoreUtils/AutomaticBackup.java
+++ app/src/main/java/com/jitender/xpensmanager/BackupAndRestoreUtils/AutomaticBackup.java
@@ -3,17 +3,22 @@ package com.jitender.xpensmanager.BackupAndRestoreUtils;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.util.Log;
+
+import androidx.annotation.RequiresApi;
 
 public class AutomaticBackup extends BroadcastReceiver {
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d("AutomaticBackup","Recieved Intent");
         context.startService(new Intent(context,BackupService.class));
-        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED") || intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED) ) {
-           AutomaticBackupManager manager = new AutomaticBackupManager();
-           manager.setAutomaticBackup(context);
+        AutomaticBackupManager manager = new AutomaticBackupManager();
+        manager.setAutomaticBackup(context);
+        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            manager.setAutomaticBackup(context);
         }
     }
 }
